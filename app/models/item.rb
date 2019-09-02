775269c49fcff12b267bd4dce498a5be4694cb34
Giancarlo <gpbene@gmail.com>
2019-09-02 16:57:49 +0200

    relation has_many in model item for orders

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
 	has_many :join_table_cart_items
 	has_many :carts, through: :join_table_cart_items
+	has_many :join_table_order_items
+  has_many :orders, through: :join_table_order_items
 end
