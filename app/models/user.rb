4aeb6dd6b932cbd0e2622394f086f19f396354af
Giancarlo <gpbene@gmail.com>
2019-09-02 17:10:50 +0200

    delete migration add cart to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  belongs_to :cart
+  has_one :cart
   has_many :orders
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -64,11 +64,8 @@ ActiveRecord::Schema.define(version: 2019_09_02_145210) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "cart_id"
-    t.index ["cart_id"], name: "index_users_on_cart_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "users", "carts"
 end
