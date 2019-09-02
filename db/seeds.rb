3c3fd71ef69fbfbccfab9a4c2608e743babf1af4
iroshiva <jean.weil@protonmail.com>
2019-09-02 13:02:36 +0200

    beginning seed items + download picture cat for items seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Item.destroy_all
+
+20.times do
+	Item.create()
+end
