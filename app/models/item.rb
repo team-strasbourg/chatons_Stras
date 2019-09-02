3084b4364cd10073de26fe5b53e1319096c38c30
Carsano <damien.fabiano@gadz.org>
2019-09-02 22:00:52 +0200

    validates title in Item

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,8 @@ class Item < ApplicationRecord
 	has_many :carts, through: :join_table_cart_items
 	has_many :join_table_order_items
   has_many :orders, through: :join_table_order_items
+
+	validates :title,
+						presence: true,
+						length: { minimum: 2}
 end
