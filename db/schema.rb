fc1970905f75fa67062e89b4e553027264c6a3b2
Nadrojz <jor.perrin@gmail.com>
2019-09-02 19:02:24 +0200

    show item page added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_09_02_145210) do
 
   # These are extensions that must be enabled in order to support this database
@@ -65,11 +64,8 @@ ActiveRecord::Schema.define(version: 2019_09_02_145210) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "cart_id"
-    t.index ["cart_id"], name: "index_users_on_cart_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "users", "carts"
 end
