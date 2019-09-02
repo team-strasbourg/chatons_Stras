ddfbab1b546954004e17114db8e9acec9c49863b
Giancarlo <gpbene@gmail.com>
2019-09-02 18:15:00 +0200

    generate join_table_cart_items controller + routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   
+
   devise_for :users
 
   resources :users
   resources :items
   resources :carts
   resources :orders
+  resources :join_table_cart_items
 
 
   root "items#index"
