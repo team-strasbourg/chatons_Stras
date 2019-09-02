cfbce72c0ae4645c0e6acc1bcd3867724ef8e344
Giancarlo <gpbene@gmail.com>
2019-09-02 16:55:34 +0200

    belongs to in migration JoinTableOrderItem

--- db/migrate/20190902145210_create_join_table_order_items.rb
+++ db/migrate/20190902145210_create_join_table_order_items.rb
@@ -1,7 +1,8 @@
 class CreateJoinTableOrderItems < ActiveRecord::Migration[5.2]
   def change
     create_table :join_table_order_items do |t|
-
+      t.belongs_to :item, index: true
+      t.belongs_to :order, index: true
       t.timestamps
     end
   end
