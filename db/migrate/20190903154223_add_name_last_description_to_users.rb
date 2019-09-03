edcd77f99d96c19766a2fe02c5e0d492e7e2f3f9
Giancarlo <gpbene@gmail.com>
2019-09-03 17:44:15 +0200

    create migration adding columns to users

---
+++ db/migrate/20190903154223_add_name_last_description_to_users.rb
@@ -0,0 +1,7 @@
+class AddNameLastDescriptionToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :description, :text
+  end
+end
