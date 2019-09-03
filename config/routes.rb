5a8a357cc792d92db544548a3328d1e61f0524b7
Carsano <damien.fabiano@gadz.org>
2019-09-03 14:02:52 +0200

    add controller devise for adding cart when user created

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   
 
 
-  devise_for :users
+  devise_for :users, controllers: {
+      registrations: 'users/registrations'
+  }
 
   resources :users
   resources :items do
@@ -19,6 +21,7 @@ Rails.application.routes.draw do
   resources :orders
 
   resources :join_table_order_items
+
 
 
 
