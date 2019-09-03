c02e13c177b7c64669e1e18572d0d266c421353e
Carsano <damien.fabiano@gadz.org>
2019-09-03 17:08:55 +0200

    stripe works until the controller create

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -19,7 +19,7 @@ class ChargesController < ApplicationController
                                        description: 'Rails Stripe customer',
                                        currency: 'usd',
                                    })
-
+    puts "#"*60
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to new_charge_path
