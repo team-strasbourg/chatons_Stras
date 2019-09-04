652c6968410dfe3be949cd5a7f54c99f47f634c8
Carsano <damien.fabiano@gadz.org>
2019-09-04 15:44:21 +0200

    update has_one_attached

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
 	has_many :carts, through: :join_table_cart_items
 	has_many :join_table_order_items
   has_many :orders, through: :join_table_order_items
-	has_one :avatar
+	has_one_attached :avatar
 
 	validates :title,
 						presence: true,
