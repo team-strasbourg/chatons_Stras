d484ad700d6bd86fd00bcc6f3d06e0de644df92b
Carsano <damien.fabiano@gadz.org>
2019-09-04 10:42:38 +0200

    add template edit items and method in controller

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -15,15 +15,28 @@ module Admin
     end
 
     def edit
+      @item = Item.find(params[:id])
     end
 
     def update
+      @item = Item.find(params[:id])
+      if item.update(item_params)
+        redirect_to admin_root_path
+      else
+        render 'edit'
+      end
     end
 
     def destroy
       Item.destroy(params[:id])
       flash[:success] = "Item successfully deleted"
       redirect_to admin_items_path
+    end
+
+    private
+
+    def item_params
+      params[:item].permit(:title, :description, :price, :image_url)
     end
   end
 end
