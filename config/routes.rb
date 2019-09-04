4ba97151115f6b839133d0886f7fc8fa52f9e109
Boomybox <bijeard.cassandra@gmail.com>
2019-09-04 10:23:54 +0200

    added routes for static_pages

--- config/routes.rb
+++ config/routes.rb
@@ -26,5 +26,10 @@ Rails.application.routes.draw do
   root "items#index"
 
 
+# routes for static_pages :
+
+  get "/contact", to: "static_pages#contact"
+  get "/about", to: "static_pages#about"
+  get "/use_terms", to: "static_pages#terms_of_use"
 
 end
