fe418e4112b2a063af36fb85b9533991d6dee1f2
Giancarlo <gpbene@gmail.com>
2019-09-04 12:08:06 +0200

    find image urls in deliver_order

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,8 +7,18 @@ class UserMailer < ApplicationMailer
     mail(to: @user.email, subject: 'Bienvenue chez nous !') 
   end
 
-  def deliver_order(user)
+  def deliver_order(user, order)
+    
+    @jtoi_array = JoinTableOrderItem.where(order_id:order.id).to_a
+    puts '4' * 60
+    puts @jtoi_array
+    @jtoi_array.each do |o|
+      puts o.item.image_url
+    end
+    
+    attachments.inline['image.jpg'] = File.read('/path/to/image.jpg')
     @user = user 
+
     mail(to: @user.email, subject: 'Ta commande') 
   end
 
