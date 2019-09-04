98cdcb6531ed6b95307fa9c96d318ba5b642e356
Carsano <damien.fabiano@gadz.org>
2019-09-04 18:15:47 +0200

    update production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,7 @@ Rails.application.configure do
 
   # config.action_mailer.perform_deliveries = true
   # config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.default_url_options = { :host => 'chatons-stras-staging.herokuapp.com' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
