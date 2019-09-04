9bd715da0451436af92158e6475725f2316cf322
Carsano <damien.fabiano@gadz.org>
2019-09-04 19:13:17 +0200

    comment mails commande

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -21,8 +21,8 @@ class ChargesController < ApplicationController
                                        currency: 'usd',
                                    })
     @order.update(paid: true)
-    UserMailer.deliver_order(current_user, @order).deliver_now
-    AdminMailer.notify_admin(current_user, @order).deliver_now
+    # UserMailer.deliver_order(current_user, @order).deliver_now
+    # AdminMailer.notify_admin(current_user, @order).deliver_now
     redirect_to root_path
   rescue Stripe::CardError => e
     flash[:error] = e.message
