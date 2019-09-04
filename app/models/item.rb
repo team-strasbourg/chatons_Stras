238530e919ade61b96e781d7af9ac35e3dc810f0
Carsano <damien.fabiano@gadz.org>
2019-09-04 16:31:59 +0200

    add show avatar in show

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,7 @@ class Item < ApplicationRecord
 
   validates :title, presence: true, length: { minimum: 2 }
   validates :description, presence: true, length: { minimum: 30 }
-  validates :price, presence: true, numericality: { in: 1.00..1000.00 }
+  validates :price, presence: true, numericality: { in: 0.99..999.99 }
   validates :image_url, presence: true
 
   def avatar_attached? # for admin table
