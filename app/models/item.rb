919afec2189d8c959bafd47a51098b835d772628
Carsano <damien.fabiano@gadz.org>
2019-09-04 15:47:33 +0200

    rubycop class item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,20 +1,13 @@
 class Item < ApplicationRecord
-	has_many :join_table_cart_items
-	has_many :carts, through: :join_table_cart_items
-	has_many :join_table_order_items
+  has_many :join_table_cart_items
+  has_many :carts, through: :join_table_cart_items
+  has_many :join_table_order_items
   has_many :orders, through: :join_table_order_items
-	has_one_attached :avatar
+  has_one_attached :avatar
 
-	validates :title,
-						presence: true,
-						length: { minimum: 2}
-	validates :description,
-						presence: true,
-						length: { minimum: 30}
-	validates :price,
-						presence: true,
-						numericality:{in: 1.00..1000.00}
-	validates :image_url,
-						presence: true
+  validates :title, presence: true, length: { minimum: 2 }
+  validates :description, presence: true, length: { minimum: 30 }
+  validates :price, presence: true, numericality: { in: 1.00..1000.00 }
+  validates :image_url, presence: true
 
 end
