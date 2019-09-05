670e30534a045ad43d052b595236051fba662918
Boomybox <bijeard.cassandra@gmail.com>
2019-09-05 16:44:20 +0200

    add pictures to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,7 @@ JoinTableOrderItem.destroy_all
   Item.create(title:Faker::Quote.robin,
               description:Faker::Quote.matz,
               price:Faker::Number.decimal(l_digits: 2),
-              image_url: ['item_cat.jpg', 'item_cat2.jpg', 'chaton_2.jpg', 'chaton_1.jpg', 'item_cat3'].sample)
+              image_url: ['item_cat.jpg', 'item_cat2.jpg', 'chaton_2.jpg', 'chaton_1.jpg', 'item_cat3', 'item_cat4', 'item_cat5', 'item_cat6'].sample)
 end
 Item.find_each(&:save)
 puts 'Items created'
