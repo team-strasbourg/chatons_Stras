9afeb9569c1068861f122a09ead33b9ddd15c2a8
Carsano <damien.fabiano@gadz.org>
2019-09-05 18:08:39 +0200

    update mailers

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,37 +1,35 @@
+# frozen_string_literal: true
+
 class UserMailer < ApplicationMailer
   default from: 'no-reply@chatons.fr'
- 
+
   def welcome_email(user)
-    @user = user 
-    @url  = 'https://chatons-stras-staging.herokuapp.com' 
-    mail(to: @user.email, subject: 'Bienvenue chez nous !') 
+    @user = user
+    @url  = 'https://chatons-stras-staging.herokuapp.com'
+    mail(to: @user.email, subject: 'Bienvenue chez nous !')
   end
 
   def deliver_order(user, order)
-    @jtoi_array = JoinTableOrderItem.where(order_id:order.id).to_a
+    @jtoi_array = JoinTableOrderItem.where(order_id: order.id).to_a
 
     @images = []
 
-    @jtoi_array.each do |o| 
-      if o.item.avatar_attached?
-        @image = o.item.avatar
-      else 
-        @image = o.item.image_url
-      end
-      
+    @jtoi_array.each do |o|
+      @image = if o.item.avatar_attached?
+                 o.item.avatar
+               else
+                 o.item.image_url
+               end
+
       @images << @image
       if o.item.avatar_attached?
-        attachments.inline["#{@image}"]
+        attachments.inline[@image.to_s]
       else
-        attachments.inline["#{@image}"] = File.read("app/assets/images/#{@image}")
+        attachments.inline[@image.to_s] = File.read("app/assets/images/#{@image}")
       end
-      
-    
     end
-    
-    @user = user 
 
-    mail(to: @user.email, subject: 'Ta commande') 
+    @user = user
+    mail(to: @user.email, subject: 'Ta commande')
   end
-
 end
